5c3d33ac49834b42ae9cb58533016dbaa27947a1
Omar Tamer <omartamer@timeout.com>
2012-09-02 22:36:37 +0100

    update for latest deploy

--- templates/download-artifact-from-nexus.rb
+++ templates/download-artifact-from-nexus.rb
@@ -2,6 +2,17 @@
 LINK_OR_DIR=/opt/socialapp/
 RELEASE=`date +%m%d%Y%H%M%S`
 
+killall -v java
+
+if [ -f $LINK_OR_DIR\RUNNING_PID ]
+then
+   sudo kill -9 `cat RUNNING_PID`
+   sudo rm RUNNING_PID
+fi
+
+
+mv $LINK_OR_DIR/socialapp-LATEST $LINK_OR_DIR/socialapp-LATEST-$RELEASE 
+
 if [ ! -d "$LINK_OR_DIR" ]; then 
     if [ ! -L "$LINK_OR_DIR" ]; then
         # It is a symlink!
@@ -21,7 +32,12 @@ fi
 -a socialapp:socialapp:LATEST \
 -e jar \
 -c dist \
--o /opt/socialapp/socialapp-0.1.108-dist.zip \
+-o /opt/socialapp/socialapp-LATEST.zip \
 -r releases \
 -n <%= url %> \
 -u <%= username %> -p '<%= password %>'
+
+
+unzip /opt/socialapp/socialapp-LATEST.zip
+
+
