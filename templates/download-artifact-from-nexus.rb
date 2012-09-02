c002d9a62df948087dbd12ab2136685aeaf682b6
Omar Tamer <omartamer@timeout.com>
2012-09-02 23:20:01 +0100

    update for latest deploy

--- templates/download-artifact-from-nexus.rb
+++ templates/download-artifact-from-nexus.rb
@@ -1,9 +1,20 @@
+
 LINK_OR_DIR=/opt/socialapp
+RELEASES=$LINK_OR_DIR"/releases
 RELEASE=`date +%m%d%Y%H%M%S`
+
+if [ ! -d "$LINK_OR_DIR" ]; then 
+echo NO $LINK_OR_DIR dir - creating....
+        # It's a directory!
+        # Directory command goes here.
+        mkdir -p "$LINK_OR_DIR"
+		mkdir -p "$LINK_OR_DIR"/backup
+		mkdir -p "$RELEASES"
+fi
+
 LAST_RELEASE=`ls -x /opt/socialapp|awk '{print $1}'`
 killall -v java
 CURRENT=$LINK_OR_DIR/current
-RELEASES=$LINK_OR_DIR"/releases
 
 if [ -f $LINK_OR_DIR/RUNNING_PID ]
 then
@@ -15,14 +26,6 @@ rm -f $CURRENT
 
 #rm -f $LINK_OR_DIR/socialapp-LAST_RELEASE $LINK_OR_DIR/socialapp-LATEST-$RELEASE 
 
-if [ ! -d "$LINK_OR_DIR" ]; then 
-echo NO $LINK_OR_DIR dir - creating....
-        # It's a directory!
-        # Directory command goes here.
-        mkdir -p "$LINK_OR_DIR"
-		mkdir -p "$LINK_OR_DIR"/backup
-		mkdir -p "$RELEASES"
-fi
 
 
 mkdir -p $RELEASES/$RELEASE
