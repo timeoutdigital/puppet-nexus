35ebd93a3074422d66e196864c3fd0440c984abe
Omar Tamer <omartamer@timeout.com>
2012-09-02 21:38:52 +0100

    template update

--- templates/download-artifact-from-nexus.rb
+++ templates/download-artifact-from-nexus.rb
@@ -1,4 +1,23 @@
- /opt/nexus-script/download-artifact-from-nexus.sh  \
+
+LINK_OR_DIR=/opt/socialapp/
+RELEASE=`date +%m%d%Y%H%M%S`
+
+if [ ! -d "$LINK_OR_DIR" ]; then 
+    if [ ! -L "$LINK_OR_DIR" ]; then
+        # It is a symlink!
+        # Symbolic link specific commands go here.
+        ln -s "$LINK_OR_DIR" 
+    else
+        # It's a directory!
+        # Directory command goes here.
+        mkdir -p "$LINK_OR_DIR"
+		mkdir -p "$LINK_OR_DIR"/backup
+		mkdir -p "$LINK_OR_DIR"/releases
+
+    fi
+fi
+
+/opt/nexus-script/download-artifact-from-nexus.sh  \
 -a socialapp:socialapp:LATEST \
 -e jar \
 -c dist \
